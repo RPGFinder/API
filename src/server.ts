c09821c53b0b1f3ffc65b84b54d8b9c7a92a2d41
Mateus Nadal <nlmatt07@hotmail.com>
2020-11-19 16:52:39 -0300

    ESLINT

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
+});
+
+const port = 3333;
+app.listen(3333, () => {
+  return console.log(`Server running on port ${port}`);
+});
